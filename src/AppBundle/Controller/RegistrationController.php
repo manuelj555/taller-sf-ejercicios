80324d38abfd53f7379e2ae896eec728ca2de4ba
manuelj555 <programador.manuel@gmail.com>
2016-09-30 21:02:09 -0400

    Creada accion para el formulario

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -3,11 +3,18 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 /**
  * @author Manuel Aguirre
  */
 class RegistrationController extends Controller
 {
-
+    /**
+     * @Route("/register", name="registration")
+     */
+    public function formAction(Request $request)
+    {
+    }
 }
