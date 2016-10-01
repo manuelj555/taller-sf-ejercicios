1d4673d65f1fbcac776d170ad32f831bd284fb49
manuelj555 <programador.manuel@gmail.com>
2016-09-30 21:02:37 -0400

    Creando el formulario en el controlador

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\Type\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -16,5 +17,10 @@ class RegistrationController extends Controller
      */
     public function formAction(Request $request)
     {
+        $form = $this->createForm(RegistrationType::class);
+        
+        return $this->render('registration/form.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
